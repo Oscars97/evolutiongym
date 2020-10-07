666a5dd89593076a19db002cf04a8d458df8f61e
OscarSanchezM97 <oscarsanchezmeza97@gmail.com>
2020-10-07 16:22:51 -0600

    button map and aligment

--- js/index.js
+++ js/index.js
@@ -1,5 +1,5 @@
 'use strict'
-var btn_show_map = document.querySelector('.show-map');
+var btn_show_map = document.querySelector('.btn-show-map');
 var map = document.querySelector('.map');
 
 btn_show_map.addEventListener('click', ()=>{map.classList.toggle('show-element');})
