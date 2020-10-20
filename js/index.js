74a34527413c1a39f01aaa91304e6b1c89937058
OscarSanchezM97 <oscarsanchezmeza97@gmail.com>
2020-10-20 13:17:46 -0600

    Location and footer modified, including social media and working with the map

--- js/index.js
+++ js/index.js
@@ -1,8 +1,19 @@
 'use strict'
-var btn_show_map = document.querySelector('.btn-show-map');
-var map = document.querySelector('.map');
 
-btn_show_map.addEventListener('click', ()=>{map.classList.toggle('show-element');})
+var btnShow = document.querySelector('.btn-show-map');
+var myMap = document.querySelector('.map');
+
+btnShow.addEventListener('click', function(){
+    
+    if (myMap.className=='.show-element') {
+        myMap.className('');
+        console.log('I dont want to work as you want');
+    }else{
+        myMap.className='show-element';
+        btnShow.innerHTML = 'Ocultar mapa';
+    }
+});
+
 
 
 
