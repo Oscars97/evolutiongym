035f45bc71b5ae80ffd291c245e15ce3019ab3e7
OscarSanchezM97 <oscarsanchezmeza97@gmail.com>
2020-10-21 13:24:52 -0600

    a little animation for the services section

--- js/index.js
+++ js/index.js
@@ -1,20 +1,15 @@
 'use strict'
-
-var btnShow = document.querySelector('.btn-show-map');
-var myMap = document.querySelector('.map');
-
-btnShow.addEventListener('click', function(){
-    
-    if (myMap.className=='.show-element') {
-        myMap.className('');
-        console.log('I dont want to work as you want');
-    }else{
-        myMap.className='show-element';
-        btnShow.innerHTML = 'Ocultar mapa';
-    }
-});
+// VARIABLES
+var btnServices = document.querySelector('#link-services');
 
 
+// EVENTS
+btnServices.addEventListener("click", animateServices);
 
+
+// FUNCTIONS
+function animateServices() {
+    document.getElementById('services').classList.add('animate');
+};
 
 
